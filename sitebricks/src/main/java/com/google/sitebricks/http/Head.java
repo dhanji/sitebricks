c3ca933670ab3c805eb9e870ccae2697e8ddc8fc
dhanji <dhanji@c7392d14-1f41-11de-a108-cd2f117ce590>
2011-02-01 10:31:15 +0000

    Added Head and Trace method annotations as per JF Arcand's request.
    git-svn-id: http://google-sitebricks.googlecode.com/svn/trunk@222 c7392d14-1f41-11de-a108-cd2f117ce590

--- sitebricks/src/main/java/com/google/sitebricks/http/Head.java
+++ sitebricks/src/main/java/com/google/sitebricks/http/Head.java
@@ -10,6 +10,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Get {
+public @interface Head {
     String value() default "";
 }
