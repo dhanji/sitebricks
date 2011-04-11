ffa8b429690f01748b3071a1972a53b4c6e3645e
Tamas Cservenak <tamas@cservenak.net>
2011-04-11 16:07:20 +0200

    http://code.google.com/p/google-sitebricks/issues/detail?id=70
    SLF4J works fine when logger field is declared in current class, but misses logger when it's declared in some superclass.
    
    Modified Slf4jInjectionTypeListener to support this while keeping good logger name, and also extended test.

--- slf4j/src/test/java/com/google/sitebricks/slf4j/Slf4jIntegrationTest.java
+++ slf4j/src/test/java/com/google/sitebricks/slf4j/Slf4jIntegrationTest.java
@@ -9,30 +9,47 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
 
-
 /**
  * @author dhanji@gmail.com (Dhanji R. Prasanna)
  */
 public class Slf4jIntegrationTest {
-  @Test
-  public final void testLoggerInjection() {
-    Injector injector = Guice.createInjector(new AbstractModule() {
-      @Override
-      protected void configure() {
-        install(new Slf4jModule());
-      }
-    });
+    @Test
+    public final void testLoggerInjection() {
+	Injector injector = Guice.createInjector(new AbstractModule() {
+	    @Override
+	    protected void configure() {
+		install(new Slf4jModule());
+	    }
+	});
 
-    AService instance = injector.getInstance(AService.class);
+	// simple case
+	AService instance = injector.getInstance(AService.class);
 
-    assertNotNull(instance.log);
-    assertEquals(AService.class.getName(), instance.log.getName());
+	assertNotNull(instance.log);
+	assertEquals(AService.class.getName(), instance.log.getName());
 
-    // Expect no exception thrown.
-    instance.log.debug("Works!");
-  }
+	// Expect no exception thrown.
+	instance.log.debug("Works!");
 
-  public static class AService {
-    Logger log;
-  }
+	// inherited logger
+	TheService inheritedInstance = injector.getInstance(TheService.class);
+
+	assertNotNull(inheritedInstance.log);
+	assertEquals(TheService.class.getName(),
+		inheritedInstance.log.getName());
+
+	// Expect no exception thrown.
+	inheritedInstance.log.debug("Works!");
+    }
+
+    public static class AbstractService {
+	Logger log;
+    }
+
+    public static class TheService extends AbstractService {
+    }
+
+    public static class AService {
+	Logger log;
+    }
 }
