0dbeadbb0937bb46aae1f59047829b421d9f1798
Dhanji R. Prasanna <dhanji@gmail.com>
2011-04-19 13:16:08 +1000

    Apache license information & beginnigns of options module

---
+++ sitebricks-options/src/main/java/com/google/sitebricks/options/OptionsModule.java
@@ -0,0 +1,17 @@
+package com.google.sitebricks.options;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * @author dhanji@gmail.com (Dhanji R. Prasanna)
+ */
+public class OptionsModule extends AbstractModule {
+  public OptionsModule(String[] commandLine) {
+
+  }
+
+
+  @Override
+  protected final void configure() {
+  }
+}
