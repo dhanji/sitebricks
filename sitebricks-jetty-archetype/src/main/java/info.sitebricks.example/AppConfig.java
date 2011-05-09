2f62ab3e2d4861df34149a2443b86776354a8c1f
Dhanji R. Prasanna <dhanji@gmail.com>
2011-05-10 09:56:21 +1000

    Jetty archetype

---
+++ sitebricks-jetty-archetype/src/main/java/info.sitebricks.example/AppConfig.java
@@ -0,0 +1,29 @@
+package info.sitebricks.example;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.sitebricks.SitebricksModule;
+import info.sitebricks.example.web.HomePage;
+
+/**
+ * The main configuration for a sitebricks servlet app. This class is typically
+ * registered as a &lt;listener&gt; in web.xml.
+ *
+ * @author dhanji@gmail.com (Dhanji R. Prasanna)
+ */
+public class AppConfig extends GuiceServletContextListener {
+  @Override
+  protected Injector getInjector() {
+    return Guice.createInjector(new SitebricksModule() {
+      @Override
+      protected void configureSitebricks() {
+
+        // Tell Sitebricks to scan the contents of package info.sitebricks.example.web
+        // and ALL of its child packages for bricks, pages and other sitebricks artifacts.
+        scan(HomePage.class.getPackage());
+
+      }
+    });
+  }
+}
---
+++ sitebricks-jetty-archetype/src/main/java/info.sitebricks.example/web/HomePage.java
@@ -0,0 +1,32 @@
+package info.sitebricks.example.web;
+
+import com.google.sitebricks.At;
+import com.google.sitebricks.Visible;
+import com.google.sitebricks.http.Get;
+
+/**
+ * The home page that our users will see at the top level URI "/".
+ * <p>
+ * This page is created once per request and has "no scope" in Guice
+ * terminology. See the <a href="http://code.google.com/p/google-guice">Guice wiki</a>
+ * for details.
+ *
+ * @author dhanji@gmail.com (Dhanji R. Prasanna)
+ */
+@At("/")
+public class HomePage {
+
+  @Visible
+  String message;
+
+  @Get
+  void showHome() {
+    // This is where you would normally fetch stuff from a database, for example.
+    message = "Hello from Sitebricks!";
+  }
+
+  public boolean getShouldShow() {
+    // Always show our message.
+    return true;
+  }
+}
