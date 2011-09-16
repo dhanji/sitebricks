c5ecdc371cdc0c3adc40fd4384049a594e6fd9c5
Dhanji R. Prasanna <dhanji@gmail.com>
2011-09-16 17:29:56 +1000

    Bugfix: IMAP Folder did not have equals/hashcode, triggering non-deterministic failures

--- sitebricks-mail/src/main/java/com/google/sitebricks/mail/imap/Folder.java
+++ sitebricks-mail/src/main/java/com/google/sitebricks/mail/imap/Folder.java
@@ -15,4 +15,21 @@ public class Folder {
   public String getName() {
     return name;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Folder)) return false;
+
+    Folder folder = (Folder) o;
+
+    if (name != null ? !name.equals(folder.name) : folder.name != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
 }
