d4e62beb66724a5509f8e386a1744866240635d5
Usman Ismail <usman@techtraits.ca>
2011-12-16 12:17:06 -0500

    In the hidden method filter we wrap the original request before trying to read the parameter, if the parameter is not found we return the original (wrapped) request and not the one for which we have consumed the input stream. This will fix issue 45 http://code.google.com/p/google-sitebricks/issues/detail?id=45

--- sitebricks/src/main/java/com/google/sitebricks/HiddenMethodFilter.java
+++ sitebricks/src/main/java/com/google/sitebricks/HiddenMethodFilter.java
@@ -1,102 +1,142 @@
 
 package com.google.sitebricks;
 
-import com.google.inject.Singleton;
-import com.google.sitebricks.rendering.Strings;
-import net.jcip.annotations.Immutable;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Locale;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import java.io.IOException;
-import java.util.Locale;
+
+import net.jcip.annotations.Immutable;
+
+import com.google.inject.Singleton;
+import com.google.sitebricks.rendering.Strings;
 
 /**
- * Enables browsers making a simulated PUT and DELETE requests. Currently browsers support making
- * GET and POST requests. This {@link javax.servlet.Filter} checks if a hidden field is set and
- * renames HTTP method, retrieved via {@link javax.servlet.http.HttpServletRequest#getMethod()} to a method
- * set in the hidden field
- *
+ * Enables browsers making a simulated PUT and DELETE requests. Currently
+ * browsers support making GET and POST requests. This
+ * {@link javax.servlet.Filter} checks if a hidden field is set and renames HTTP
+ * method, retrieved via
+ * {@link javax.servlet.http.HttpServletRequest#getMethod()} to a method set in
+ * the hidden field
+ * 
  * @author Peter Knego
  */
 @Immutable
 @Singleton
 class HiddenMethodFilter implements Filter {
 
-  private static final String FILTER_DONE_SUFFIX = "__done";
-  private static final String HIDDEN_FIELD_NAME = "hiddenFieldName";
+	private static final String FILTER_DONE_SUFFIX = "__done";
+	private static final String HIDDEN_FIELD_NAME = "hiddenFieldName";
 
-  /**
-   * Name of the hidden field. Shared across the entire App. Is guarded by
-   * init so no danger of visibility issues.
-   */
-  static String hiddenFieldName = "__sitebricks__action";
-  private String filterDoneAttributeName;
+	/**
+	 * Name of the hidden field. Shared across the entire App. Is guarded by
+	 * init so no danger of visibility issues.
+	 */
+	static String hiddenFieldName = "__sitebricks__action";
+	private String filterDoneAttributeName;
 
-  public void init(FilterConfig filterConfig) throws ServletException {
-    String param = filterConfig.getInitParameter(HIDDEN_FIELD_NAME);
-    if (param != null) {
-      hiddenFieldName = param;
-    }
+	public void init(FilterConfig filterConfig) throws ServletException {
+		String param = filterConfig.getInitParameter(HIDDEN_FIELD_NAME);
+		if (param != null) {
+			hiddenFieldName = param;
+		}
 
-    // Request attribute name to signal that filtering was already done in a request
-    filterDoneAttributeName = hiddenFieldName + FILTER_DONE_SUFFIX;
-  }
-
-
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-      throws IOException, ServletException {
-
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
-
-    // check if filtering was already done on this request
-    if (httpRequest.getAttribute(filterDoneAttributeName) != null) {
-      // Filtering done, forward to another filter in chain
-      filterChain.doFilter(httpRequest, response);
-
-    } else {
-      httpRequest.setAttribute(filterDoneAttributeName, Boolean.TRUE);
-
-      try {
-    	HttpServletRequestWrapper originalRequest = new HttpServletRequestWrapper(httpRequest);
-        String methodName = httpRequest.getParameter(this.hiddenFieldName);
-
-        if ("POST".equalsIgnoreCase(httpRequest.getMethod()) && !Strings.empty(methodName)) {
-          String methodNameUppercase = methodName.toUpperCase(Locale.ENGLISH);
-          HttpServletRequest wrapper = new HttpMethodRequestWrapper(methodNameUppercase, httpRequest);
-          filterChain.doFilter(wrapper, response);
-        } else {
-
-          // Filtering done, forward to another filter in chain
-          filterChain.doFilter(originalRequest, response);
-        }
-      } finally {
-        // Remove the filterDone attribute for this request.
-        request.removeAttribute(filterDoneAttributeName);
-      }
-    }
-  }
-
-  public void destroy() {
-  }
+		// Request attribute name to signal that filtering was already done in a
+		// request
+		filterDoneAttributeName = hiddenFieldName + FILTER_DONE_SUFFIX;
+	}
 
 
-  private static class HttpMethodRequestWrapper extends HttpServletRequestWrapper {
-    private final String method;
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException,
+			ServletException {
 
-    public HttpMethodRequestWrapper(String method, HttpServletRequest request) {
-      super(request);
-      this.method = method;
-    }
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-    @Override
-    public String getMethod() {
-      return this.method;
-    }
-  }
+		// check if filtering was already done on this request
+		if (httpRequest.getAttribute(filterDoneAttributeName) != null) {
+			// Filtering done, forward to another filter in chain
+			filterChain.doFilter(httpRequest, response);
+
+		} else {
+			httpRequest.setAttribute(filterDoneAttributeName, Boolean.TRUE);
+
+			try {
+				ServletInputStream inputStream = httpRequest.getInputStream();
+				byte[] reqBytes = new byte[httpRequest.getContentLength()];
+				inputStream.read(reqBytes);
+
+				// Making the input stream available again because we have
+				// already read bytes
+				HttpServletRequestWrapper wrappedRequest = getWrappedRequest(httpRequest, reqBytes);
+				String methodName = wrappedRequest.getParameter(this.hiddenFieldName);
+
+				if ("POST".equalsIgnoreCase(httpRequest.getMethod()) && !Strings.empty(methodName)) {
+					String methodNameUppercase = methodName.toUpperCase(Locale.ENGLISH);
+					HttpServletRequest wrapper = new HttpMethodRequestWrapper(methodNameUppercase, httpRequest);
+					filterChain.doFilter(wrapper, response);
+				} else {
+
+					// Making the input stream available again fix for issue 45
+					wrappedRequest = getWrappedRequest(httpRequest, reqBytes);
+					// Filtering done, forward to another filter in chain
+					filterChain.doFilter(httpRequest, response);
+				}
+			} finally {
+				// Remove the filterDone attribute for this request.
+				request.removeAttribute(filterDoneAttributeName);
+			}
+		}
+	}
+
+	private HttpServletRequestWrapper getWrappedRequest(HttpServletRequest httpRequest, final byte[] reqBytes)
+			throws IOException {
+
+
+		final ByteArrayInputStream byteInput = new ByteArrayInputStream(reqBytes);
+		return new HttpServletRequestWrapper(httpRequest) {
+
+			@Override
+			public ServletInputStream getInputStream() throws IOException {
+
+				ServletInputStream sis = new ServletInputStream() {
+					@Override
+					public int read() throws IOException {
+						return byteInput.read();
+					}
+				};
+
+				return sis;
+			}
+		};
+
+	}
+
+	public void destroy() {
+	}
+
+
+	private static class HttpMethodRequestWrapper extends HttpServletRequestWrapper {
+		private final String method;
+
+		public HttpMethodRequestWrapper(String method, HttpServletRequest request) {
+			super(request);
+			this.method = method;
+		}
+
+		@Override
+		public String getMethod() {
+			return this.method;
+		}
+	}
+
+
 }
