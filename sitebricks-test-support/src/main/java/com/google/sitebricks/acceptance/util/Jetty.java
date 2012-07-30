eef22d071a3db987528e197e5233b9153dd22d91
Jason van Zyl <jason@sonatype.com>
2012-07-30 15:22:12 -0400

    o allow the Injector and classloader to be passed into the Jetty instance so that it can be used by the WebAppContext

--- sitebricks-test-support/src/main/java/com/google/sitebricks/acceptance/util/Jetty.java
+++ sitebricks-test-support/src/main/java/com/google/sitebricks/acceptance/util/Jetty.java
@@ -3,18 +3,30 @@ package com.google.sitebricks.acceptance.util;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.webapp.WebAppContext;
 
+import com.google.inject.Injector;
+
 /**
  * @author Dhanji R. Prasanna (dhanji@gmail.com)
+ * @author Jason van Zyl
  */
 public class Jetty {
+  
+  public static final String INJECTOR_KEY = "@_INJECTOR_@";
   private static final String APP_NAME = "/sitebricks";
-
   private final Server server;
+
+  public Jetty(String path, int port, ClassLoader classLoader, Injector injector) {
+    WebAppContext webAppContext = new WebAppContext(path, APP_NAME);
+    webAppContext.setClassLoader(classLoader);
+    webAppContext.getServletContext().setAttribute(INJECTOR_KEY, injector);
+    server = new Server(port);
+    server.addHandler(webAppContext);
+  }  
 
   public Jetty(String path, int port) {
     this(new WebAppContext(path, APP_NAME), port);
   }  
-  
+    
   public Jetty(WebAppContext webAppContext, int port) {
     server = new Server(port);
     server.addHandler(webAppContext);
