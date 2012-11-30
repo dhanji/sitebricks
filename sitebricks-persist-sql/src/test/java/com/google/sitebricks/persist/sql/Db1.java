e53f982e714dc569654c8e44cb7dc55acca1119a
Dhanji R. Prasanna <dhanji@gmail.com>
2012-11-30 14:05:21 +1100

    Added tests for multiple stores in the same injector

---
+++ sitebricks-persist-sql/src/test/java/com/google/sitebricks/persist/sql/Db1.java
@@ -0,0 +1,14 @@
+package com.google.sitebricks.persist.sql;
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author dhanji@gmail.com (Dhanji R. Prasanna)
+ */
+@BindingAnnotation
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Db1 {
+}
---
+++ sitebricks-persist-sql/src/test/java/com/google/sitebricks/persist/sql/SqlMultipleStoreIntegrationTest.java
@@ -0,0 +1,109 @@
+package com.google.sitebricks.persist.sql;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.sitebricks.persist.EntityStore;
+import com.google.sitebricks.persist.PersistAopModule;
+import com.google.sitebricks.persist.Persister;
+import com.google.sitebricks.persist.Work;
+import com.jolbox.bonecp.BoneCPConfig;
+import org.testng.annotations.BeforeMethod;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
+
+/**
+ * @author dhanji@gmail.com (Dhanji R. Prasanna)
+ */
+public class SqlMultipleStoreIntegrationTest {
+  public static final String A_NAME = "Jason Van Zyl";
+
+  public static class SqlSaver {
+    @Inject
+    Provider<Sql> sql;
+
+    @Work @Db2
+    public void make() {
+      sql.get().execute("insert into my_table (id, name) values (1, @name)",
+          ImmutableMap.<String, Object>of("name", A_NAME));
+    }
+
+    @Work @Db2
+    public String find() {
+      List<Map<String,Object>> list = sql.get().list("select * from my_table");
+      assertFalse(list.isEmpty());
+      return list.iterator().next().get("name").toString();
+    }
+  }
+
+  private BoneCPConfig config1;
+  private BoneCPConfig config2;
+
+  @BeforeMethod
+  public final void pre() {
+    config1 = new BoneCPConfig();
+    config1.setJdbcUrl("jdbc:hsqldb:mem:muldb1;sql.syntax_mys=true");
+    config1.setUser("sa");
+    config1.setPassword("");
+    config1.setPartitionCount(1);
+    config1.setMaxConnectionsPerPartition(2);
+
+    config2 = new BoneCPConfig();
+    config2.setJdbcUrl("jdbc:hsqldb:mem:muldb2;sql.syntax_mys=true");
+    config2.setUser("sa");
+    config2.setPassword("");
+    config2.setPartitionCount(1);
+    config2.setMaxConnectionsPerPartition(2);
+  }
+
+  private static void createTable(Injector injector, Class<? extends Annotation> selector) {
+    Persister persister = injector.getInstance(Key.get(Persister.class, selector));
+    persister.call(new Persister.InWork() {
+      @Override
+      public Object perform(EntityStore es) throws Throwable {
+        ((Sql) es.delegate()).execute("create table my_table (id integer, name text not null)");
+        return null;
+      }
+    });
+
+    final AtomicBoolean tableExists = new AtomicBoolean();
+    persister.call(new Persister.InWork() {
+      @Override
+      public Object perform(EntityStore es) throws Throwable {
+        tableExists.set(((Sql) es.delegate()).tableExists("my_table"));
+        return null;
+      }
+    });
+
+    assertTrue(tableExists.get());
+  }
+
+//  @Test
+  public final void storeAndRetrieve() {
+    SqlModule db1Module = new SqlModule(Db1.class, config1);
+    SqlModule db2Module = new SqlModule(Db2.class, config2);
+    Injector injector = Guice.createInjector(
+        db1Module,
+        db2Module,
+        new PersistAopModule(db1Module),
+        new PersistAopModule(db2Module));
+    createTable(injector, Db1.class);
+    createTable(injector, Db2.class);
+
+    SqlSaver saver = injector.getInstance(SqlSaver.class);
+
+    saver.make();
+
+    assertEquals(A_NAME, saver.find());
+  }
+}
