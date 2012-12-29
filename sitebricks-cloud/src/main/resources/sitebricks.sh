04019ffdf6232fb5cf94f17edc9272d78a6dee76
Dhanji R. Prasanna <dhanji@gmail.com>
2012-12-29 15:29:34 +0530

    Added SB command-line bootstrapper

---
+++ sitebricks-cloud/src/main/resources/sitebricks.sh
@@ -0,0 +1,10 @@
+SB_VERSION=0.8.8-SNAPSHOT
+SB_JAR="$(eval echo ~$USERNAME)/.m2/repository/com/google/sitebricks/sitebricks-cloud/${SB_VERSION}/sitebricks-cloud-${SB_VERSION}.jar"
+
+# Download the latest SB cloud module from sonatype snapshots (if necessary)
+if [ ! -f $SB_JAR ] || [ "$1" == "selfupdate" ]; then
+  mvn dependency:get -Dartifact=com.google.sitebricks:sitebricks-cloud:$SB_VERSION -DrepoUrl=https://oss.sonatype.org/content/repositories/google-snapshots
+fi
+
+# run it!
+java -cp $SB_JAR com.google.sitebricks.cloud.Cloud $*
