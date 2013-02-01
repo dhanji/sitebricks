fb3bafa1f62b653b4ccee47e79e39e3c1e6cca17
Eric Charles <eric@aos.io>
2013-02-01 18:14:19 +0000

    remove unneeded import

--- sitebricks/src/main/java/com/google/sitebricks/i18n/Message.java
+++ sitebricks/src/main/java/com/google/sitebricks/i18n/Message.java
@@ -1,9 +1,7 @@
 package com.google.sitebricks.i18n;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 /**
  * Annotate localization interface methods with this to describe the purpose and value of
