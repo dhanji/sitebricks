e8988408392247e1ea7c190ea4ec76ecdaa42c11
Eric Charles <eric@aos.io>
2013-02-01 18:11:15 +0000

    add public and Target on ResourceBundle annotation to try to fix jenkins build

--- sitebricks/src/main/java/com/google/sitebricks/i18n/Message.java
+++ sitebricks/src/main/java/com/google/sitebricks/i18n/Message.java
@@ -1,13 +1,16 @@
 package com.google.sitebricks.i18n;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Annotate localization interface methods with this to describe the purpose and value of
  * an internationalized message.
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
 public @interface Message {
   String message();
   String description() default "";
