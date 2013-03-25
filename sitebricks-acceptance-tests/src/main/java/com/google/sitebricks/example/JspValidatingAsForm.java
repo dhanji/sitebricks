46d66140fac83b7af4ef690e14af2ebf6490fe66
Eric Charles <eric@qubitdigital.com>
2013-03-25 17:05:20 +0000

    remove uneeded DAO in JspValidatingForm

--- sitebricks-acceptance-tests/src/main/java/com/google/sitebricks/example/JspValidatingAsForm.java
+++ sitebricks-acceptance-tests/src/main/java/com/google/sitebricks/example/JspValidatingAsForm.java
@@ -1,10 +1,7 @@
 package com.google.sitebricks.example;
 
-import javax.inject.Inject;
-
 import com.google.sitebricks.Show;
 import com.google.sitebricks.acceptance.util.Jetty;
-import com.google.sitebricks.example.dao.SimpleDao;
 import com.google.sitebricks.example.model.Person;
 import com.google.sitebricks.http.As;
 import com.google.sitebricks.http.Get;
@@ -16,9 +13,6 @@ import com.google.sitebricks.transport.Form;
 @Show("/JspValidatingAsForm.jsp")
 @ResourceBundle(value = JspValidatingAsForm.ErrorMessages.class)
 public class JspValidatingAsForm {
-    
-    @Inject
-    private SimpleDao dao;
     
     public JspValidatingAsForm() {
     }
